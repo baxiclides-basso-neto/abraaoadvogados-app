0dfcfef0f8c942a15582a615129df2ce0c6f21cb
NetoBasso <baxnetodev@gmail.com>
2023-03-20 11:03:04 -0300

    First commit

---
+++ src/components/header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <section>Logo</section>
+      <section>
+        <h1>
+          Se você é mamãe e recebeu salário maternidade nos últimos 5 anos,
+          FIQUE ATENTA!{" "}
+        </h1>
+        <h3>
+          É possível que o benefício tenha sido pago em um valor menor pelo
+          governo. Isso significa que você pode ter valores a serem devolvidos.
+        </h3>
+        <p>
+          Não se preocupe, estamos aqui para ajudar! Basta clicar no botão verde
+          e entrar em contato com nossa equipe para obter mais informações.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Header;
