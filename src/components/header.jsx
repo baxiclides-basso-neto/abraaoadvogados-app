926d0ea6a3fe24df6b4849b7985e28e9331892f8
NetoBasso <baxnetodev@gmail.com>
2023-03-20 21:45:10 -0300

    Component Header under construction

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,50 @@
 import React from "react";
-import { Header } from "@/styles/HeaderStyle";
-
+import {
+  BackgroundImage,
+  Header,
+  HeaderDescSection,
+  HeaderWrapper,
+  LogoSection,
+} from "@/styles/HeaderStyle";
+import Image from "next/image";
 
 const HeaderComponent = () => {
-  
-    return (
-
-        
-    <Header>
-
-      <section>Logo</section>
-      <section>
-        <h1>
-          Se você é mamãe e recebeu salário maternidade nos últimos 5 anos,
-          FIQUE ATENTA!
-        </h1>
-        <h3>
-          É possível que o benefício tenha sido pago em um valor menor pelo
-          governo. Isso significa que você pode ter valores a serem devolvidos.
-        </h3>
-        <p>
-          Não se preocupe, estamos aqui para ajudar! Basta clicar no botão verde
-          e entrar em contato com nossa equipe para obter mais informações.
-        </p>
-      </section>
+  return (
+    <HeaderWrapper>
+      <BackgroundImage />
+      <Header>
+        <LogoSection>
+          <h1>Logo</h1>
+        </LogoSection>
+        <HeaderDescSection>
+          <h1>
+            Se você é mamãe e recebeu salário maternidade nos últimos 5 anos,
+            FIQUE ATENTA!
+          </h1>
+          <p>
+            É possível que o benefício tenha sido pago em um valor menor pelo
+            governo. Isso significa que você pode ter valores a serem
+            devolvidos.
+          </p>
+          <h3>
+            Não se preocupe, estamos aqui para ajudar! Basta clicar no botão
+            verde e falar com nossa equipe!
+          </h3>
+          <button>
+            <div>
+              <Image
+                src="/whatsappicon.png"
+                alt="WhatsApp"
+                width={30}
+                height={30}
+                style={{ backgroundColor: "red" }}
+              />
+              Fale conosco agora pelo WhatsApp
+            </div>
+          </button>
+        </HeaderDescSection>
       </Header>
-      
+    </HeaderWrapper>
   );
 };
 
--- src/styles/HeaderStyle.js
+++ src/styles/HeaderStyle.js
@@ -1,7 +1,92 @@
 import styled from "styled-components";
+import HeaderBgImg from "../../public/headerImg.jpg";
+
+export const HeaderWrapper = styled.div`
+  position: relative;
+`;
+
+export const BackgroundImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.75);
+    z-index: -1;
+  }
+
+  background: url(${HeaderBgImg.src});
+  background-size: cover;
+  background-position: center center;
+`;
 
 export const Header = styled.header`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  height: 100%;
+
+  text-align: center;
+  padding: .5rem;
+  box-sizing: border-box;
+  z-index: 1;
+`;
+
+export const LogoSection = styled.section`
+  position: relative;
+  z-index: 2;
+  color: black;
+
+  >h1{
+    color: gray;
+  }
+`;
+
+export const HeaderDescSection = styled.section`
+  position: relative;
+  z-index: 2;
+  color: black;
+
+  >h1{
+    color: #fff;
+    text-align: left;
+    line-height: 90%;
+  }
+
+  >h3{
+    color: #fff;
+    text-align: left;
+    line-height: 90%;
+  }
+
+  p{
+    color: #fff;
+    text-align: left;
+    line-height: 90%;
+  }
+
+  button{
+    width: 90%;
+    padding: .5rem;
+    border-radius: 4px;
+    border: none;
+    background-color: lightgreen;
+    color: #fff;
+    font-weight: bold;
+    font-size:.9rem;
+    
+    div{
+      display: flex;
+      align-items: center;
+      gap:.5rem;
+    }
+  }
 `;
